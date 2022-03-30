404567f4a0881c34d9895d597b89d60f72b8dec8
Paul Badger <paul@moderndevice.com>
2022-03-29 23:34:50 -0400

    :wq
    first commit:`

---
+++ WindTunnelControllerAndTester02/fan.h
@@ -0,0 +1,70 @@
+/* Fan Control functions and PID for temperature control */
+
+#define fanTachPin 2  // yellow wire
+#define fanPWMpin  5  // orange wire (blue on fan) wire timer 3 (pin 2, 3, 5)
+#define fanPotNumber 0
+#define PWMCALIBRATION 2  // adjust PWM to make up for new wind tunnel setup
+#define ISRSMOOTHSAMPLES 32
+
+volatile unsigned long fanTach, ISRperiod, lastInterrTime; // for ISR
+volatile int newISRdata;                                   // for ISR
+int  fanPWM, smoothArrayCounter = 0;
+unsigned long periodSmoothArray[ISRSMOOTHSAMPLES], arrayTotal;
+unsigned int tach;
+/********** TODO implement this PID scheme and see if you can stabilize fan *********/
+double fanDoubleTach, fanDoublePWM, fanDoubleSetpoint = 272.0;
+double Fan_Kp = 110, Fan_Ki = 3.0, Fan_Kd = 1.0;
+
+//PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+// PID fanPID(&fanDoubleTach, &fanDoublePWM, &innerLoopSetpoint, H_Kp, H_Ki, H_Kd,  DIRECT);
+
+fanISR() {  // this reads the fan tach
+   /* there is a glitch in this routine that reports 12 uSta every so often ?!? 
+   try hardware debounce - schmidt trigger?*/
+  volatile unsigned long tempTime = micros();
+  ISRperiod = tempTime - lastInterrTime;
+  lastInterrTime = tempTime;
+  newISRdata = 1;
+}
+
+void initFan() {
+  pinMode(fanTachPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(fanTachPin), fanISR, CHANGE);
+  TCCR3B = (TCCR3B & 0xF8) | 0x01 ;  // pwm pin must be 9 or 10! increase PWM freq
+  Serial.println(" intFan ");
+
+  //  fanPID.SetOutputLimits(130, 190);
+  //  fanPID.SetSampleTime(300); // ms sample rate computes at this rate //used to be 2000
+  //  fanPID.SetMode(AUTOMATIC); // turn the PID on
+}
+
+
+void doFan() {
+  static unsigned long lastTimePWM, lastTimeTach;
+
+
+  if (millis() - lastTimePWM > 40) { //time to PWM speed
+    lastTimePWM = millis();
+    fanPWM = ((pots[fanPotNumber] / 4) / 5) * 5 ; // quantize to 5's
+    if (fanPWM > 255) fanPWM = 255;
+    // analogWrite(fanPWMpin, fanPWM + PWMCALIBRATION); // note calibration fudge
+    analogWrite(fanPWMpin, fanPWM + PWMCALIBRATION); // note calibration fudge
+
+    // Serial.println(ISRperiod);
+
+    if ( newISRdata && (ISRperiod > 20) ) {  // debounces some short period tach glitches on fan
+      arrayTotal = arrayTotal - periodSmoothArray[smoothArrayCounter];
+      periodSmoothArray[smoothArrayCounter] = ISRperiod;
+      arrayTotal = arrayTotal + ISRperiod;
+      smoothArrayCounter = (smoothArrayCounter + 1) % ISRSMOOTHSAMPLES;
+      int smoothTach = arrayTotal / ISRSMOOTHSAMPLES;
+      tach = (1000000 / smoothTach ); 
+
+     // Serial.print(ISRperiod); Serial.print("\t smooth"); Serial.print(smoothTach); Serial.print("\t tach"); Serial.println(tach);
+      newISRdata = 0;
+    }
+
+
+    // Serial.print("\t tempTach " ),  Serial.print(tempTach) ; Serial.print("\t tach " ),  Serial.println(tach) ;
+  }
+}
---
+++ WindTunnelControllerAndTester02/interfaceBd.h
@@ -0,0 +1,56 @@
+/* Control Board (pots) code, for reading 8 pots and 4 momentary switches
+   on an interface board attached to the Arduino mega
+*/
+#include <Adafruit_MCP3008.h>
+Adafruit_MCP3008 adc;
+
+int pots[8];
+int momSw; //variable for momentary switches
+
+void initInterfaceBd() {  // this is the control board with the pots
+  Serial.begin(57600);
+  while (!Serial);
+
+  pinMode(9, INPUT_PULLUP);
+  pinMode(10, INPUT_PULLUP);
+  pinMode(11, INPUT_PULLUP);
+  pinMode(12, INPUT_PULLUP);
+  // (sck, mosi, miso, cs);
+  adc.begin(52, 51, 50, 53); //Mega SPI pins
+  Serial.println( "int interface");
+ 
+}
+
+void readInterfaceBd() {
+
+  static unsigned long lastTime;
+    
+  if (millis() - lastTime > 200 ) { // 20 times a sec
+    lastTime = millis();
+
+    for (int chan = 0; chan < 8; chan++) {
+      pots[chan] = adc.readADC(chan);
+    }
+
+    momSw = 0;
+    for (int pin = 9; pin < 13; pin++) {  // read 4 switches and encode binary
+      momSw = (momSw * 2) + !digitalRead(pin); // switches active low state
+    }
+
+/*
+
+    for (int chan = 0; chan < 8; chan++) {
+      Serial.print(pots[chan]);
+      Serial.print("\t");
+    }
+
+
+    for (int chan = 0; chan < 4; chan++) {
+      Serial.print(momSw);
+      Serial.print("\t");
+    }
+    Serial.println("");
+*/
+
+  }
+}
